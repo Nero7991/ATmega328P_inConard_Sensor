62a6550f0c172dd48cd241232801215094f14a2b
Oren Collaco <orencollaco97@gmail.com>
2019-03-30 11:19:09 +0530

    Initial commit

---
+++ ATmega328P_inConard_Sensor/ProjectDef.h
@@ -0,0 +1,16 @@
+/*
+ * ProjectDef.h
+ *
+ * Created: 3/30/2019 11:15:35 AM
+ *  Author: orencollaco
+ */ 
+
+
+#ifndef PROJECTDEF_H_
+#define PROJECTDEF_H_
+
+
+
+
+
+#endif /* PROJECTDEF_H_ */
---
+++ ATmega328P_inConard_Sensor/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * ATmega328P_inConard_Sensor.cpp
+ *
+ * Created: 3/30/2019 11:11:13 AM
+ * Author : orencollaco
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
